0ddd151fbdf038bc82b298d762d5e3f3e8753590
Jeffrey Rohlman <jeffreyrohlman@jeffreys-mbp.attlocal.net>
2017-07-28 17:52:56 -0500

    Completed layout of calculator

---
+++ ConversionCalculator/ConversionCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConversionCalculator
+//
+//  Created by Jeffrey Rohlman on 7/28/17.
+//  Copyright © 2017 Jeffrey Rohlman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
